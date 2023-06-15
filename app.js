c7fa894e0816d8a42ebd212a43849eb5eaf95eb3
IsaaMaster <songisaac17@gmail.com>
2023-06-14 20:41:16 -0700

    link updates

---
+++ app.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+    $('a').attr('target', '_blank');
+
+    $('#name').fadeIn(3000);
+    $('#about').fadeIn(5000);
+    
+    /***
+     * $('#name').fadeIn(3000).promise().done(function() {
+        $('#about').fadeIn(5000);
+        }); 
+     */
+    
+    
+}); 
