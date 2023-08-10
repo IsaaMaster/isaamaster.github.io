283f6939471efbe4f5fa9a88a9ef52b16aa10ebf
IsaaMaster <songisaac17@gmail.com>
2023-08-09 21:03:12 -0700

    updates

--- app.js
+++ app.js
@@ -1,46 +1,3 @@
-"use strict";
-
-let words = document.querySelectorAll(".word");
-words.forEach((word) => {
-  let letters = word.textContent.split("");
-  word.textContent = "";
-  letters.forEach((letter) => {
-    let span = document.createElement("span");
-    span.textContent = letter;
-    span.className = "letter";
-    word.append(span);
-  });
-});
-
-let currentWordIndex = 0;
-let maxWordIndex = words.length - 1;
-words[currentWordIndex].style.opacity = "1";
-
-let rotateText = () => {
-  let currentWord = words[currentWordIndex];
-  let nextWord =
-    currentWordIndex === maxWordIndex ? words[0] : words[currentWordIndex + 1];
-  // rotate out letters of current word
-  Array.from(currentWord.children).forEach((letter, i) => {
-    setTimeout(() => {
-      letter.className = "letter out";
-    }, i * 80);
-  });
-  
-  // reveal and rotate in letters of next word
-  nextWord.style.opacity = "1";
-  Array.from(nextWord.children).forEach((letter, i) => {
-    letter.className = "letter behind";
-    setTimeout(() => {
-      letter.className = "letter in";
-    }, 340 + i * 80);
-  });
-  currentWordIndex =
-    currentWordIndex === maxWordIndex ? 0 : currentWordIndex + 1;
-};
-
-rotateText();
-setInterval(rotateText, 4000);
 
 
 
@@ -50,9 +7,9 @@ $(document).ready(function() {
 
 
 
-    $('a').attr('target', '_blank');
+    $('#links a').attr('target', '_blank');
 
-    var name = $('#name').animate({opacity: 1}, 1000).promise();
+    var name = $('#me').animate({opacity: 1}, 1000).promise();
     
 
     $.when(name).done(function() {
@@ -69,6 +26,12 @@ $(document).ready(function() {
 
       }); 
     }); 
+
+
+   $('#noteworks').click(function() {
+      var modal = $('#noteWorksModal'); 
+      modal.modal('show');
+   });     
 
     
 
