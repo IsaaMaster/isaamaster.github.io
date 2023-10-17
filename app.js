378499a82529ae447b4fc817b5f11e4098770ecd
IsaaMaster <songisaac17@gmail.com>
2023-10-16 21:42:17 -0700

    fade-in on scrool animations

--- app.js
+++ app.js
@@ -44,14 +44,21 @@ $(document).ready(function() {
    });     
 
 
-    
+   const observer = new IntersectionObserver((entries) => {
+      entries.forEach(entry => {
+         if (entry.isIntersecting) {
+            entry.target.classList.add('show');
+         } else {
+            entry.target.classList.remove('show');
+         }
+      });
+   }); 
 
-    
-     
-    
-    
-}); 
+   const hiddenElements = document.querySelectorAll('.hidden');
+   hiddenElements.forEach(element => {
+      observer.observe(element);
+   });
 
 
 
-
+});
